21e30179298f952d5dec154764218a0b5522db97
sanju-creator <70715411+sanju-creator@users.noreply.github.com>
2024-06-02 04:09:31 +0000

    add data types summary in container

---
+++ 01_basics/dataTypesSummary.js
@@ -0,0 +1,19 @@
+let myYoutubeName = "sanju"
+
+let anotherName = myYoutubeName
+anotherName = "Pallabi"
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let userOne = {
+    name: "Riya",
+    emailId: "riya@google.com"
+}
+
+let userTwo = userOne
+
+userTwo.emailId = "pallabi@google.com"
+
+console.log(userOne.emailId);
+console.log(userTwo.emailId);
